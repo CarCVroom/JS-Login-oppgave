82b2b63880f5812a3ac37466b192d955094e73dc
CarCVroom <bjornarlundm@gmail.com>
2026-03-20 11:11:19 +0100

    Admin passwork
    It works.
    
    Password is "Passord1234",
    Username is "Admin5678"

--- JS/server.js
+++ JS/server.js
@@ -18,6 +18,37 @@ app.use(express.json());
 let usernames = ['', "Admin5678"];
 let passwords = ['', "Passord1234"];
 
+function hashAdminWithSalt(ADMINPASS, ADMINSALT) {
+        json_reader(filePathLocal, (err, data) => { // Kaller json_reader funksjonen
+                if (err) {
+                        console.log(err);
+                        return res.status(500).send('Error')
+                } else {
+                        const ADMINPASSWORD = crypto.scryptSync(ADMINPASS, ADMINSALT, 64).toString("hex");
+                        data[0].ADMINPASSWORD = ADMINPASSWORD;
+                        fs.writeFile(filePathLocal, JSON.stringify(data, null, 4), err => {
+                                if (err) { 
+                                        console.log(err)
+                                        return 1;
+                                }
+                        })
+                }
+        })
+
+}
+
+
+fs.readFile(filePathLocal, 'utf-8', (err, fileData) => {
+        if (err) {
+                console.log(err);
+                return err;
+        } else {
+                const data = JSON.parse(fileData);
+                hashAdminWithSalt(passwords[1], data[0].ADMINSALT)
+        }
+})
+
+
 function hashPassowrd(passwordToBeHashed) {
         const salt = crypto.randomBytes(16).toString("hex");
         const hash = crypto.scryptSync(passwordToBeHashed, salt, 64);
@@ -34,22 +65,22 @@ app.post('/new_password', (req, res) => {
         usernames[0] = req.body.username; // Henter username og password fra client
         passwords[0] = req.body.password;
         json_reader(filePathLocal, (err, data) => { // Kaller json_reader funksjonen
-        if (err) {
-                console.log(err);
-                return res.status(500).send('Error')
-        } else {
-                const { salt, hash } = hashPassowrd(passwords[0])
-                data[0].username = usernames[0]; // Klargjør data for json
-                data[0].password = hash.toString("hex");
-                data[0].salt = salt; 
-                fs.writeFile(filePathLocal, JSON.stringify(data, null, 4), err => { // Skriver til filen
-                        if (err) {
-                                console.log(err);
-                                return res.status(500).send('Error writing JSON');
-                        }
-                        res.send('JSON updated');
-                });
-        }
+                if (err) {
+                        console.log(err);
+                        return res.status(500).send('Error')
+                } else {
+                        const { salt, hash } = hashPassowrd(passwords[0])
+                        data[0].username = usernames[0]; // Klargjør data for json
+                        data[0].password = hash.toString("hex");
+                        data[0].salt = salt; 
+                        fs.writeFile(filePathLocal, JSON.stringify(data, null, 4), err => { // Skriver til filen
+                                if (err) {
+                                        console.log(err);
+                                        return res.status(500).send('Error writing JSON');
+                                }
+                                res.send('JSON updated');
+                        });
+                }
         });
 });
 
@@ -67,11 +98,15 @@ app.post('/get_password', (req, res) => {
                         const usernameGet = dataGet[0].username;
 
                         const resualt = verifyPassword(inputPassword, saltGet, hashGet)
+
+                        const ADMINSALTGET = dataGet[0].ADMINSALT;
+                        const ADMINHASHGET = dataGet[0].ADMINPASSWORD;
+
+                        const ADMINRESULATAT = verifyPassword(passwords[1], ADMINSALTGET, ADMINHASHGET);
                         
                         if (resualt && usernameGet === inputUsername) { 
                                 res.status(200).send("OK")
-                        } else if (inputPassword === dataGet[0].ADMINPASSWORD && 
-                                   inputUsername === dataGet[0].ADMINUSERNAME) {
+                        } else if (ADMINRESULATAT && inputUsername === dataGet[0].ADMINUSERNAME) {
                                 res.status(201).send("OK")
                         } else {
                                 res.status(401).send('Invalid password');
